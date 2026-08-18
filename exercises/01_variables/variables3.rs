708c3852daffb43d79ccb62c938294768b146b4d
paradoxmelody <melodymapper@gmail.com>
2026-08-19 01:12:20 +0200

    first commit for rustlings exercises

---
+++ exercises/01_variables/variables3.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x = 132;
+
+    println!("Number {x}");
+}
---
+++ exercises/01_variables/variables6.rs
@@ -0,0 +1,6 @@
+// TODO: Change the line below to fix the compiler error.
+let NUMBER = 3;
+
+fn main() {
+    println!("Number: {NUMBER}");
+}
