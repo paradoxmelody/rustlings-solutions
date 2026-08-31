2c076e3b93b0b08862fe9933f1db9dd3a6fac80b
paradoxmelody <melodymapper@gmail.com>
2026-08-31 23:51:17 +0200

    sol 14

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,17 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a >= b{
+        a
+    }else{
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("Bigger term is: {} ", bigger(10, 8));
+     
 }
 
 // Don't mind this for now :)
