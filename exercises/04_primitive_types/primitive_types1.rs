e1f3f7f4143048115764dd56cdbb958ddcaefb15
paradoxmelody <melodymapper@gmail.com>
2026-09-03 23:58:13 +0200

    no need

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -1,5 +1,5 @@
 // Booleans (`bool`)
-
+ 
 fn main() {
     let is_morning = true;
     if is_morning {
@@ -8,7 +8,7 @@ fn main() {
 
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    let is_evening = !is_morning;
     if is_evening {
         println!("Good evening!");
     }
